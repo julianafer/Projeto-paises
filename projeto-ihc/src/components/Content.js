b0b9af594b8b664c1170fa9d6f934d174d3834d8
Juliana Ferreira <101995776+julianafer@users.noreply.github.com>
2023-04-25 15:34:16 -0300

    continuação do conteúdo

--- projeto-ihc/src/components/Content.js
+++ projeto-ihc/src/components/Content.js
@@ -1,6 +1,7 @@
-import { FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material"
+import { Button, Radio, RadioGroup, FormControlLabel, FormLabel, FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material"
 import { GiAirplaneDeparture } from "react-icons/gi";
 import * as React from 'react';
+import Info from './Info'
 
 function Content() {
     const [age, setAge] = React.useState('');
@@ -9,26 +10,48 @@ function Content() {
         setAge(event.target.value);
     };
 
+    const [openInfo, setOpenInfo] = React.useState(false)
+
     return (
-        <div className="div-content">
-            <div className="passagens">
-                <TextField id="outlined-basic1" label="Origem" variant="outlined" />
-                <GiAirplaneDeparture id="aviao" />
-                <TextField id="outlined-basic2" label="Destino" variant="outlined" />
+        <div>
+            <span id="tip" onClick={() => setOpenInfo((prev) => !prev)} >i</span>
+            {
+                openInfo && <Info />
+            }
+            <div className="div-content">
+                <div className="passagens">
+                    <TextField id="outlined-basic1" label="Origem" variant="outlined" />
+                    <GiAirplaneDeparture id="aviao" />
+                    <TextField id="outlined-basic2" label="Destino" variant="outlined" />
+                </div>
+                <div id="drop-radio">
+                    <FormControl className="drop-form">
+                        <InputLabel id="demo-simple-select-label">Opções de viagem</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={age}
+                            label="Opções de viagem"
+                            onChange={handleChange}
+                        >
+                            <MenuItem>Só ida</MenuItem>
+                            <MenuItem>Ida e volta</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <FormControl className="radio">
+                    <FormLabel id="demo-row-radio-buttons-group-label">Aceita conexão:</FormLabel>
+                    <RadioGroup
+                        row
+                        aria-labelledby="demo-row-radio-buttons-group-label"
+                        name="row-radio-buttons-group"
+                    >
+                        <FormControlLabel value="female" control={<Radio />} label="Sim" />
+                        <FormControlLabel value="male" control={<Radio />} label="Não" />
+                    </RadioGroup>
+                    </FormControl>
+                </div>
+                <Button id="comprar" variant="contained">Comprar passagem</Button>
             </div>
-            <FormControl className="drop-form">
-                <InputLabel id="demo-simple-select-label">Opções de viagem</InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={age}
-                    label="Opções de viagem"
-                    onChange={handleChange}
-                >
-                    <MenuItem>Só ida</MenuItem>
-                    <MenuItem>Ida e volta</MenuItem>
-                </Select>
-            </FormControl>
         </div>
     )
 }
---
+++ projeto-ihc/src/components/Info.js
@@ -0,0 +1,9 @@
+function DropDark() {
+    return (
+        <div className="info">
+            Área onde você pode comprar passagens aéreas para o destino que desejar
+        </div>
+    )
+}
+
+export default DropDark
